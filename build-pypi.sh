c29614da705e645d98ba0403c65c33279ba56149
server <noreply@bijoux-individuels.com>
2021-09-23 22:46:03 +0200

    Pushed to pypi => pip install umlsequence2

---
+++ build-pypi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Pip packaging:
+#   https://packaging.python.org/tutorials/packaging-projects/
+#
+# Entry point:
+#   https://setuptools.readthedocs.io/en/latest/userguide/entry_point.html
+#
+# Getting a token:
+#   https://test.pypi.org/manage/account/token/
+
+set -ex
+
+rm -rf dist/ src/*.egg-info/
+python3 -m build
+python3 -m twine upload --username __token__ dist/*
